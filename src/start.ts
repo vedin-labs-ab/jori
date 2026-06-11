b0a826f668eab101c4f4863161b664653a71d295
Albin Vedin <albin-vedin@hotmail.com>
2026-06-11 09:34:35 +0200

    Remove Clerk Start middleware

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,3 @@
-import { clerkMiddleware } from "@clerk/tanstack-react-start/server"
 import { createStart } from "@tanstack/react-start"
 
-export const startInstance = createStart(() => ({
-  requestMiddleware: [clerkMiddleware()],
-}))
+export const startInstance = createStart(() => ({}))
