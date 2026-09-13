4853616a27c0a8996521692776ca41ec0db493f1
Albin Vedin <albin-vedin@hotmail.com>
2026-09-13 19:06:35 +0200

    Remove disconnected workstreams console views

--- src/console/context/workstreams/types.ts
+++
@@ -1,7 +0,0 @@
-import { type FunctionReturnType } from "convex/server"
-import { type api } from "../../../../convex/_generated/api"
-
-export type Workstreams = NonNullable<
-  FunctionReturnType<typeof api.workstreams.queries.list>
->["workstreams"]
-export type Workstream = Workstreams[number]
